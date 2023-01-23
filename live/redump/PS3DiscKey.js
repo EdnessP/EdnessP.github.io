e1a9aa0fe33772733a7112ebfb3e1729959f8b7f
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-01-23 17:35:06 +0200

    ??????

--- live/redump/PS3DiscKey.js
+++ live/redump/PS3DiscKey.js
@@ -11,8 +11,8 @@ function keyArrToInt(keyArr) {
     return key;
 }
 
-function keyIntToArr(keyInt) {
-    let key = new Uint8Array(16);
+function keyIntToArr(keyInt, arrLen) {
+    let key = new Uint8Array(arrLen);
     for (i = 15; i >= 0; i--) {
         key[i] = Number(keyInt & 0xFFn);
         keyInt >>= 8n;
@@ -20,31 +20,30 @@ function keyIntToArr(keyInt) {
     return key;
 }
 
-function keyStrToArr(keyStr) {
+function keyStrToArr(keyStr, arrLen) {
     let key = 0n;
     key |= BigInt(parseInt(keyStr.slice(0, 10), 16)) << 96n;
     key |= BigInt(parseInt(keyStr.slice(10, 18), 16)) << 64n;
     key |= BigInt(parseInt(keyStr.slice(18, 26), 16)) << 32n;
     key |= BigInt(parseInt(keyStr.slice(26, 34), 16));
-    return keyIntToArr(key);
+    return keyIntToArr(key, arrLen);
 }
 
 async function decryptDkey(input) {
-    const dKey = keyStrToArr(input);
+    const dKey = keyStrToArr(input, 32);
     const data1KeyType = await data1KeySetup();
     const data1 = await window.crypto.subtle.decrypt(data1IvType, data1KeyType, dKey);
     const output = keyArrToInt(new Uint8Array(data1, 0, 16));
     console.log(output);
-    return output;
 }
 
 async function encryptDkey(input) {
-    const data1 = keyStrToArr(input);
+    const data1 = keyStrToArr(input, 16);
+    console.log(data1);
     const data1KeyType = await data1KeySetup();
     const dKey = await window.crypto.subtle.encrypt(data1IvType, data1KeyType, data1);
     const output = keyArrToInt(new Uint8Array(dKey, 0, 16));
     console.log(output);
-    return output;
 }
 
 const data1Key = keyIntToArr(0x380BCF0B53455B3C7817AB4FA3BA90EDn);
