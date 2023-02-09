00e2ac96f26cef10ec539332ab339050b82470e2
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-02-09 21:29:16 +0200

    sims console hash

---
+++ live/strings/SimsConsoleHash.js
@@ -0,0 +1,13 @@
+// Sims Console filename lookup hash
+
+// Written by Edness   v1.0   2023-02-09
+
+function simsConsoleHash() {
+    let input = document.getElementById("simsc-hash-input").value.replace(/[\x01-\x2F]/g, "_").toUpperCase();
+    const doSims1 = document.querySelector("#sims1c-hash-toggle").checked;
+    const output = document.getElementById("simsc-hash-output");
+    if (!doSims1 && /^[0-9]/.test(input)) {
+        input = "_" + input;
+    }
+    output.value = toHex(crcCalculate(input));
+}
