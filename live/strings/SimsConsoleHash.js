40a30dc73c56284ba050f93fe7672518b1165518
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-02-11 17:49:32 +0200

    better wording ig

--- live/strings/SimsConsoleHash.js
+++ live/strings/SimsConsoleHash.js
@@ -3,7 +3,7 @@
 // Written by Edness   v1.0   2023-02-09
 
 function simsConsoleHash() {
-    let input = document.getElementById("simsc-hash-input").value.replace(/[\x01-\x2F]/g, "_").toUpperCase();
+    let input = document.getElementById("simsc-hash-input").value.toUpperCase().replace(/[^0-9A-Z]/g, "_");
     const doSims1 = document.querySelector("#sims1c-hash-toggle").checked;
     const output = document.getElementById("simsc-hash-output");
     if (!doSims1 && /^[0-9]/.test(input)) {
