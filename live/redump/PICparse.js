6f04ecee532fe53cb554e3250f5fb0f5166a2117
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-10-27 13:14:47 +0300

    minor adjustments

--- live/redump/PICparse.js
+++ live/redump/PICparse.js
@@ -1,6 +1,6 @@
 // Parses and prints Blu-ray Permanent Information & Control (PIC) data
 
-// Written by Edness   v1.6   2022-09-25 - 2023-09-18
+// Written by Edness   v1.6.1   2022-09-25 - 2023-10-27
 
 const picMaxLength = 0xC4 * 2; // Up to triple layer pressed BDs;  (0x4 + 0x40 * layers) * 2
 
@@ -41,15 +41,15 @@ function parsePic() {
                 picOutput += `Warning: Constant data mismatch for Layer ${layer}! The final output might be incorrect!\n`;
             }
         }
-        var layerSectorStart = pic.readInt(0x4) - 2;
+        var layerSectorStart = pic.readInt(0x4);
         let layerSectorEnd = pic.readInt(0x4);
         pic.seek(0x20, 1);
 
-        var layerSize = layerSectorEnd - layerSectorStart;
+        var layerSize = layerSectorEnd + 2 - layerSectorStart;
         layerbreak.push(layerbreak.at(-1) + layerSize);
         totalSize += layerSize;
         picOutput += verbose
-            ? `${identifier} - Layer ${layer} - Start: ${toHex(layerSectorStart + 2)} - End: ${toHex(layerSectorEnd)} - Size: ${toHex(layerSize)} (${layerSize} sectors, ${layerSize * 2048} bytes)\n`
+            ? `${identifier} - Layer ${layer} - Start: ${toHex(layerSectorStart)} - End: ${toHex(layerSectorEnd)} - Size: ${toHex(layerSize)} (${layerSize} sectors, ${layerSize * 2048} bytes)\n`
             : `Layer ${layer} - Size: ${layerSize} sectors, ${layerSize * 2048} bytes\n`;
     }
 
@@ -60,7 +60,7 @@ function parsePic() {
     }
 
     if (totalSize) {
-        let actualSize = (totalSize - layerSize) + (picConst.totalSectors - layerSectorStart);
+        let actualSize = (totalSize - layerSize) + (picConst.totalSectors - layerSectorStart + 2);
         picOutput += verbose
             ? `\nTotal size (Used): ${toHex(actualSize)} (${actualSize} sectors, ${actualSize * 2048} bytes) - Disc end: ${toHex(picConst.totalSectors + 1)}\n`
             + `Total size (Full): ${toHex(totalSize)} (${totalSize} sectors, ${totalSize * 2048} bytes)\n`
